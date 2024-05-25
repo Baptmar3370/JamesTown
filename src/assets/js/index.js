5138671b744000c7091964992e8526bbf296973d
Baptmar3370 <baptmar33pro@gmail.com>
2024-05-25 02:08:42 +0200

    Update index.js

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -30,9 +30,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "Je... vie...", "author": "Baptmarou" },
+            { "message": "Salut je suis du code.", "author": "Baptmarou" },
+            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Baptmarou" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
